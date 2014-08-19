fd21f5faf616fc9ede8850818462417018ee81d8
fengying0709 <765843398@qq.com>
2014-08-19 16:53:41 +0800

    2014.8.19.2
    Qzone完善

--- Qzone/js/index.js
+++ Qzone/js/index.js
@@ -1,4 +1,6 @@
 $(function(){
+
+	//天气信息隐藏
 	$("#QAWeater").hover(function(){
 		$(".QAWeaterHide").show();
 		$(this).css("border","1 dashed #54B3F3");
@@ -9,5 +11,12 @@ $(function(){
 	},function(){
 		$(".QAWeaterHide").hide();
 		$(this).css("border","");
+	});
+
+
+	//左边栏目的展开
+	$(".QACCLA").mouseover(function(){
+		$(this).hide();
+		$(this).next().show();
 	});
 });
